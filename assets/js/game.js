b0c602dd8793fb882384e8a45558c5af7ca46ac6
juliana1390 <juliana_santos@usp.br>
2022-04-23 23:35:39 -0300

    card container adjustment

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,16 +40,16 @@
 
             var card = document.querySelector("#card" + i);
             
-            card.style.left = i % 4 === 0? 5 + "px" : i % 4 * 70.85 + 5 + "px"; 
+            card.style.left = i % 4 === 0? 10 + "px" : i % 4 * 80 + 10 + "px"; 
 
             if(i < 4){
                 card.style.top = 0 + "px";
             } else if(i < 8){
-                card.style.top = 70.85 + "px";
+                card.style.top = 80 + "px";
             }else if(i < 12){
-                card.style.top = 141.7 + "px";
+                card.style.top = 160 + "px";
             } else{
-                card.style.top = 212.55 + "px";
+                card.style.top = 240 + "px";
             }
 
             // adding click event 
